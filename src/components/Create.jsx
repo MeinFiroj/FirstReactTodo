163cb86b1e587b48c88cc4527dc490db0c119692
Mein Firoj <firojs2108@gmail.com>
2025-06-03 16:23:01 +0530

    react form hook replaced by usestate hook

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,50 +1,49 @@
 import { nanoid } from "nanoid";
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const Create = (props) => {
   const alltodos = props.alltodos;
   const setalltodos = props.setalltodos;
 
-  const [title, settitle] = useState("");
-  const [isImportant, setisImportant] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
 
-  const submitHandler = (e) => {
-    e.preventDefault();
-
-    const newTodo = {
-      id: nanoid(),
-      title,
-      isImportant,
-    };
-
-    if (newTodo.title.trim().length > 1) setalltodos([...alltodos, newTodo]);
-    settitle('');
-    setisImportant(false);
+  const submitHandler = (data) => {
+    data.id = nanoid();
+    setalltodos([...alltodos, data]);
+    reset();
   };
 
   return (
     <div className="w-[50%]">
       <h1 className="text-7xl mb-10">Create Todos</h1>
-      <form className="flex flex-col items-start gap-10"
-      onSubmit={submitHandler}>
-        <input className="text-white border-b-1 w-[70%] p-2 outline-none"
-          onChange={(e) => settitle(e.target.value)}
-          value={title}
+      <form
+        className="flex flex-col items-start gap-2"
+        onSubmit={handleSubmit(submitHandler)}
+      >
+        <input
+          {...register("title", { required: "Title can not be empty" })}
+          className="text-white border-b-1 w-[70%] p-2 outline-none"
           type="text"
           placeholder="Enter task here.."
         />
-       <div>
-         <input
-         className="scale-170 mx-3 accent-green-500"
-          onChange={(e) => setisImportant(e.target.checked)}
-          checked={isImportant}
-          type="checkbox"
-          id="impCheck"
-        />
-        <label htmlFor="impCheck">Important?</label>
-       </div>
-        <button className="px-8 py-2 border rounded-lg hover:bg-white hover:text-gray-800 cursor-pointer transition duration-200 ease"
-        >Add Todo</button>
+        <small className="text-red-400">{errors?.title?.message}</small>
+        <div className="py-8">
+          <input
+            {...register("isImportant")}
+            className="scale-170 mx-3 accent-green-500"
+            type="checkbox"
+            id="impCheck"
+          />
+          <label htmlFor="impCheck">Important?</label>
+        </div>
+        <button className="px-8 py-2 border rounded-lg hover:bg-white hover:text-gray-800 cursor-pointer transition duration-200 ease">
+          Add Todo
+        </button>
       </form>
     </div>
   );
